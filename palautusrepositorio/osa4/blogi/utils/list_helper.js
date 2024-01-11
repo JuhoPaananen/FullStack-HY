0b79d050c14c15b7aab0798f20ef686f0a935e26
Juho <JuhoPaananen@users.noreply.github.com>
2024-01-11 18:36:26 +0200

    add tests and helper functions ex 4.3-4.7, reduce <3

---
+++ palautusrepositorio/osa4/blogi/utils/list_helper.js
@@ -0,0 +1,36 @@
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((max, blog) => blog.likes > max.likes ? blog : max, blogs[0])
+}
+
+const mostBlogs = (blogs) => {
+    const authors = blogs.map(blog => blog.author)
+    const uniqueAuthors = [...new Set(authors)]
+    const authorBlogs = uniqueAuthors.map(author => {
+        return { author: author, blogs: authors.filter(a => a === author).length }
+    })
+    
+    return authorBlogs.reduce((max, author) => author.blogs > max.blogs ? author : max, authorBlogs[0])
+}   
+    
+const mostLikes = (blogs) => {
+    const authors = blogs.map(blog => blog.author)
+    const uniqueAuthors = [...new Set(authors)]
+    const authorLikes = uniqueAuthors.map(author => {
+        return { author: author, likes: blogs.filter(blog => blog.author === author).reduce((sum, blog) => sum + blog.likes, 0) }
+    })
+    
+    return authorLikes.reduce((max, author) => author.likes > max.likes ? author : max, authorLikes[0])
+}
+
+
+module.exports = {
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
+}
